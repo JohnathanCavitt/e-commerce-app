1e05e5436a02d72f6a14f809ebe2fdecb0624dde
Johnathan Cavitt <johnathancavitt@gmail.com>
2021-09-08 20:52:30 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import Navbar from "./Navbar.js";
 import ItemCard from "./ItemCard.js";
+const data = require("./items.json");
+let items = data;
+console.log(typeof items);
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <h1 id="test"> </h1>
-      <ItemCard
-        imgLink="https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_awp_cu_medieval_dragon_awp_light_large.cb3b8168e59e96fd33efa9578206a2aaed036fc0.png"
-        itemName="AWP | Dragon Lore"
-        price="$456"
-      />
+      <div id="card-container">
+        {items.map((item, index) => {
+          return (
+            <ItemCard
+              key={index}
+              imgLink={item.img}
+              itemName={item.name}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
