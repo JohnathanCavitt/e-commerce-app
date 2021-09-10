2572f9cce4f78a542a8a4a87d5ac3fa4999afd1f
Johnathan Cavitt <johnathancavitt@gmail.com>
2021-09-09 23:42:11 -0700

    added button

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -7,6 +7,7 @@ function ItemCard(props) {
       <img src={props.imgLink} alt="" className="item-image" />
       <p className="item-name">{props.itemName}</p>
       <p className="price">{props.price}</p>
+      <button id="add-to-cart">Add to cart</button>
     </div>
   );
 }
