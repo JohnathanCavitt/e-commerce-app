1876b6d9719505ba9879b1dfbde355f6f5a870e2
Johnathan Cavitt <johnathancavitt@gmail.com>
2021-09-16 00:09:31 -0700

    shopping cart icon added and other additions

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./ItemCard.css";
-
+let x = "Hello World!";
 function ItemCard(props) {
+  const addToCart = (e, cartObj = "PLACEHOLDER") => {
+    console.log(cartObj);
+  };
+  let b = "hello world";
   return (
     <div className="card">
       <img src={props.imgLink} alt="" className="item-image" />
       <p className="item-name">{props.itemName}</p>
       <p className="price">{props.price}</p>
-      <button className="add-to-cart">Add to cart</button>
+      <button className="add-to-cart" onClick={(e) => addToCart(e, x)}>
+        Add to cart
+      </button>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,34 @@
 import React from "react";
 import "./Navbar.css";
+import shopping_cart from "./test4.png";
 
 function Navbar() {
   return (
-    <ul id="nav">
-      <li className="nav-item">
-        <a className="nav-link" href="default.asp">
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="news.asp">
-          News
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="contact.asp">
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="about.asp">
-          About
-        </a>
-      </li>
-    </ul>
+    <nav id="nav">
+      <ul id="nav-list">
+        <li className="nav-item" id="first">
+          <a className="nav-link" href="default.asp">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="news.asp">
+            News
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="contact.asp">
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="about.asp">
+            About
+          </a>
+        </li>
+        <input type="image" src={shopping_cart} alt="" id="shopping-cart" />
+      </ul>
+    </nav>
   );
 }
 
